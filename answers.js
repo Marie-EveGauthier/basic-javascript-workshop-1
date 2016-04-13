5869f4119a61e1c504b04bfeefa7b558c51f6a20
Marie-Eve Gauthier <gauthier.marie.eve@gmail.com>
2016-04-13 17:28:28 +0000

    function firstCharacter

--- answers.js
+++ answers.js
@@ -1,1 +1,9 @@
 // This is the beginning of my answers to the workshop
+
+//A function that takes a string and returns the first character of the string. 
+function firstCharacter(str) {
+    return str.charAt(0);
+}
+
+console.log(firstCharacter("hello"));
+console.log(firstCharacter(""));
